e54ec29d646a2f091b56c188f24c1a5fa8316fc2
bematjhb025 <bematjhb025@student.wethinkcode.co.za>
2026-02-10 10:27:50 +0200

    added objectss file

---
+++ objects.js
@@ -0,0 +1,10 @@
+let array = new Array()
+array.name = "a"
+array.age = 21
+console.log(array)
+
+let string = new String("12")
+console.log(string)
+
+let number = new Number(12)
+console.log(number)
